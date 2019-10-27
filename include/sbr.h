9013de3874a3f2d542df4e92a91978870d72304e
Noé Fernandes <noepessoa@outlook.com>
2019-10-26 22:10:15 -0300

    lista de economias decrescente funcionando

--- include/sbr.h
+++ include/sbr.h
@@ -4,6 +4,18 @@
 #include <vector>
 #include <iostream>
 
+struct Pair{
+	int leftVertex;
+	int rightVertex;
+	double saved;
+
+	Pair(){
+		leftVertex = -1;
+		rightVertex = -1;
+		saved = 0;
+	}
+};
+
 class SBR{
 	
 	public:
@@ -12,9 +24,9 @@ class SBR{
 		SBR(int n){
 			//Alocando a matriz  de adjacência.
 			V = n;
-			int** ma = new int*[V];
+			double** ma = new double*[V];
 			for(auto i(0); i < V; i++){
-				ma[i] = new int[V];
+				ma[i] = new double[V];
 				for(auto j(0); j < V; j++){
 					ma[i][j] = 0;
 				}
@@ -41,17 +53,20 @@ class SBR{
 
 	/*********************************************** FUNÇÕES DE MANIPULAÇÃO DO GRAFO *********************************************/
 	//Retorna a matriz de adjacência.
-	int** getAdjacencyMatrix(void);
+	double** getAdjacencyMatrix(void);
 	//Insere uma aresta no grafo.
-	void insertEdge(int a, int b, int cost);
+	void insertEdge(int a, int b, double cost);
 	//Remove uma aresta do grafo.
 	void removeEdge(int a, int b);
 	//Retorna o vértice adjacente de menor custo.
 	int smallestAdjacentVertex(int a);
 	//Retorna o custo do aresta.
-	int getEdgeCost(int a, int b);
+	double getEdgeCost(int a, int b);
 	//Realiza a leitura pelo da carga de alunos por parada.
 	void loadStudentsPerStop(std::string filename);
+	//Eurística de Clark & Wright na versão sequencial: encontra as rotas que cada ônibus percorrerá.
+	/*std::vector<std::string>*/ 
+	void clarkeAndWright(void);
 
 	/***************************************************** FUNÇÕES DE AUXÍLIO ****************************************************/
 	//Mostra o estado do grafo.
@@ -60,11 +75,13 @@ class SBR{
 
 	private:
 		//Matriz de adjacência contendo as distâncias entre os vértices (não adjcência representada pelo valor 0).
-		int** m;
+		double** m;
 		//Número de vértices.
 		int V;
 		//Carga a ser coletada (Número de alunos nas paradas).
 		int* C;
+		//Vector com todas as rotas formadas.
+		std::vector<std::string> rotas;
 };
 
 #endif
--- src/sbr.cpp
+++ src/sbr.cpp
@@ -3,12 +3,13 @@
 #include <limits>
 #include <fstream>
 #include <string>
+#include <bits/stdc++.h> 
 
-int** SBR::getAdjacencyMatrix(void){
+double** SBR::getAdjacencyMatrix(void){
 	return m;
 }
 
-void SBR::insertEdge(int a, int b, int cost = 0){
+void SBR::insertEdge(int a, int b, double cost = 0){
 	m[a][b] = cost;
 	m[b][a] = cost;
 }
@@ -18,7 +19,7 @@ void SBR::removeEdge(int a, int b){
 	m[b][a] = 0;
 }
 
-int SBR::getEdgeCost(int a, int b){
+double SBR::getEdgeCost(int a, int b){
 	return m[a][b];
 }
 
@@ -56,9 +57,56 @@ void SBR::loadStudentsPerStop(std::string filename){
   	}
 }
 
+bool comparePair(Pair p1, Pair p2) { 
+    return p1.saved > p2.saved; 
+} 
+
+
 void SBR::showStudentsPerStop(void){
 	for(auto i(0); i < V; i++){
 		std::cout << C[i] << " ";
 	}
 	std::cout << std::endl;
+}
+
+/*std::vector<std::string>*/void SBR::clarkeAndWright(void){
+	//Vetor de economias.
+	double** E = new double*[V];
+
+	for(auto i(0); i < V; i++){
+		E[i] = new double[V];
+		for(auto j(0); j < V; j++){
+			E[i][j] = 0;
+		}
+	}
+
+	//Encontrando os valores de economia para cada pares de nós.
+	for(auto i(0); i < V; i++){
+		for(auto j(0); j < V; j++){
+			if(i != 0 and j != 0 and i != j){
+				E[i][j] = m[0][i] + m[0][j] - m[i][j];
+			}
+		}
+	}
+
+	/****************************Ordenando cada par de vértices por sua economia de forma decrescente****************************/
+
+	//Criando matriz com valor de economia por par, vertice extremo esquerdo e vertice extremo direito.
+	std::vector<Pair> pairsList;
+
+	for(auto i(1); i < V; i++){
+		for(auto j(i + 1); j < V; j++){
+			Pair pair;
+			pair.saved = E[i][j];
+			pair.leftVertex = i;
+			pair.rightVertex = j;
+			pairsList.push_back(pair);
+		}
+	}
+
+	std::sort(pairsList.begin(), pairsList.end(), comparePair);
+
+	for(unsigned int i(0); i < pairsList.size(); i++){
+		std::cout << pairsList[i].saved << " " << pairsList[i].leftVertex << " " << pairsList[i].rightVertex << std::endl;
+	}
 }
